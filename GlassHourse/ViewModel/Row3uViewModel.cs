6038a38cf7d16d6205ba2811f7f892cc9b8726e1
gamegear <gamegear.tw@gmail.com>
2016-03-20 13:28:33 +0800

    first

---
+++ GlassHourse/ViewModel/Row3uViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GlassHourse.ViewModel
+{
+    public class Row3uViewModel
+    {
+        public string Title { get; set; }
+        public string SubTitle { get; set; }
+        public string picPath { get; set; }
+    }
+}
